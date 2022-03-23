e9366a2a6042acf682d02a382f6dee877e1c1ecc
Oliver <oliverperezp87@gmail.com>
2022-03-23 16:57:15 +0000

    Se sube el proyecto terminado a la rama feature_1 #1

---
+++ recuperacionEts/src/main/java/es/iespuerto/principal/Oliver.java
@@ -0,0 +1,41 @@
+package es.iespuerto.principal;
+
+public class Oliver {
+
+   private nombre;
+   private apellido1;
+   private apellido2;
+
+   /*
+   Constructor por defecto
+    */
+
+
+   public Oliver{}
+
+/*
+Constructor con un parametro
+ */
+
+
+   public Oliver(String nombre){}
+
+    /*
+    Constructor con dos parametros
+     */
+
+
+   public Oliver(String nombre,String apellido1){}
+
+/*
+Contructor con tres parametros
+ */
+
+   public Oliver(String nombre, String apellido1, String apellido2) {
+      nombre = nombre;
+      apellido1 = apellido1;
+      apellido2 = apellido2;
+
+
+
+}
